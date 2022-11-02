ed51150903dcb11cc1702614445d822344b694b6
Matheus Ferreira <matheus.44300@gmail.com>
2022-11-02 11:32:28 -0300

    Added script base

---
+++ script.sql
@@ -0,0 +1,35 @@
+-- Active: 1667399386701@@127.0.0.1@3306
+CREATE DATABASE apae;
+USE apae;
+
+CREATE TABLE news(
+    id_news INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(45) NOT NULL,
+    text LONGTEXT NOT NULL,
+    image_path VARCHAR(200) NOT NULL,
+    created_at_utc DATETIME NOT NULL,
+    is_enabled TINYINT(1) NOT NULL DEFAULT 1
+);
+
+CREATE TABLE product(
+    id_product INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    description LONGTEXT NOT NULL,
+    price DECIMAL(8, 5) NOT NULL,
+    image_path VARCHAR(200) NOT NULL,
+    is_enabled TINYINT(1) NOT NULL DEFAULT 1
+);
+
+CREATE TABLE donations(
+    id_donation INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    title VARCHAR(45) NOT NULL,
+    description VARCHAR(45) NOT NULL,
+    is_enabled TINYINT(1) NOT NULL DEFAULT 1 
+);
+
+CREATE TABLE bazaar_info(
+    id_bazaar_info INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    title VARCHAR(45) NOT NULL,
+    description VARCHAR(200) NOT NULL,
+    is_enabled TINYINT(1) NOT NULL DEFAULT 1
+);
