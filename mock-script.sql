da98f042927135dba9f11998e976a9f57126fffc
Matheus Ferreira <matheus.44300@gmail.com>
2022-11-04 23:24:38 -0300

    Added mock script

---
+++ mock-script.sql
@@ -0,0 +1,19 @@
+-- Criar produtos
+
+INSERT INTO products (name, description, price, image_path) VALUES 
+('Açaí', 'Um açaí top de qualidade duvidosa, o qual pode te deixar com dor de barriga', 18, ''),
+('Blusa', 'Uma blusa pra vc', 35.50, ''),
+('Jeans', 'Uma jeans de mendigo para você conquistar as gatinhas', 70.50,''),
+('Moletom', 'Um moletom de otaku virgem', 199.90,''),
+('Sorvete', 'Sorvete pra te fazer ficar gripado', 10, ''),
+('Action Figure', 'Uma action figure de otaku virjão', 200, ''),
+('Rexona', 'Ele nunca te abandona, sua mulher sim', 18, ''),
+('Tenys Pe', 'Pra tirar seu chulé desgraçado', 16,''),
+('Jogos', 'Sei lá, são jogos pra gente esquisita', 13.59, ''),
+('Xadrez', 'Jogo para você que é burro e quer ficar inteligente', 24.69, ''),
+('Smartphone', 'Um telefone pra vc postar merda no twitter', 1499.99, ''),
+('Nokia Tijolão', 'Pode usar pra bater em alguem', 69, ''),
+('Coquinha 2L', 'Huuummm coquinha', 12, ''),
+('Coquinha 1L', 'Uma coquinha, só que menor', 8, ''),
+('Monitor', 'Um monitor', 799.99, ''),
+('Chaveiro', 'Chaveiro pra vc carregar chaves', 1, '');
