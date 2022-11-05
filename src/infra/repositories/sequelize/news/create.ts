0f5c641ffe83b2cb5a690bdcb1655c95c1b1f286
Caique0Araujo <caique.ara19@gmail.com>
2022-11-05 12:03:39 -0300

    fix: fixed news date insert

--- src/infra/repositories/sequelize/news/create.ts
+++ src/infra/repositories/sequelize/news/create.ts
@@ -4,6 +4,7 @@ import { News as news} from "../../../dataSource/sequelize/news";
 
 export class CreateNewsRepositorySequelize implements CreateNewsRepository{
     async create(newNews: News): Promise<News> {
+        newNews.created_at_utc = Date.now()
         return await news.create(newNews);
     }
 }
