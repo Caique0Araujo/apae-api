4f69527c43bf1bb920c85addad5a7967e9131dfc
Caique0Araujo <caique.ara19@gmail.com>
2022-11-05 11:47:32 -0300

    refactor: change products/all return columns

--- src/infra/repositories/sequelize/product/getAll.ts
+++ src/infra/repositories/sequelize/product/getAll.ts
@@ -4,6 +4,6 @@ import Products from "../../../dataSource/sequelize/products";
 
 export class GetProductsRepositorySequelize implements GetProductsRepository{
     async getAll(): Promise<Product[]> {
-        return await Products.findAll({raw: true, where: {is_enabled: true }}); 
+        return await Products.findAll({raw: true, where: {is_enabled: true }, attributes: ['id_product', 'name', 'price', 'image_path']}); 
     }
 }
