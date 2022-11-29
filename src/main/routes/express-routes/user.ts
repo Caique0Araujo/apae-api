c278bf8e09748019f0395e73281ca5f7a2e7de35
Caique0Araujo <caique.ara19@gmail.com>
2022-11-29 16:01:43 -0300

    Add middleware to create user endpoint

--- src/main/routes/express-routes/user.ts
+++ src/main/routes/express-routes/user.ts
@@ -3,10 +3,12 @@ import { adaptRoute } from '../../adapters/expressRoutes/adapter';
 import { loginController } from '../../factories/controllers/auth/login';
 import { createUserController } from '../../factories/controllers/user/create';
 import { getUserByIdController } from '../../factories/controllers/user/getById';
+import { authenticateRoute } from '../../../presentation/middlewares/auth';
 
 const router = Router();
 
 router.post('/login', adaptRoute(loginController()));
+router.use(authenticateRoute);
 router.post('/create', adaptRoute(createUserController()));
 router.get('/byId/:id', adaptRoute(getUserByIdController()));
 
