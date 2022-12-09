a0eb7b4db9b0b8b9fa3ea569da4af4d9658baf1b
Caique0Araujo <caique.ara19@gmail.com>
2022-12-09 09:19:43 -0300

    adapt getall news to send file bytes

--- src/presentation/controllers/news/getAll.ts
+++ src/presentation/controllers/news/getAll.ts
@@ -2,13 +2,18 @@ import { defaultError } from './../http';
 import { GetNewsUseCase } from '../../../domain/useCases/news/getAll';
 import { HttpResponse, ok } from '../http';
 import { Controller } from './../controller';
+import fs from 'fs'
 export class GetNewsController implements Controller{
   constructor(
     private readonly getNewsUseCase: GetNewsUseCase
   ){}
   async handle(data: any): Promise<HttpResponse<any>> {
       try {
-        const news = await this.getNewsUseCase.load();
+        const news:any = await this.getNewsUseCase.load();
+        news.map((news) => {
+          news.image_path = fs.readFileSync(news.image_path)
+
+        })
         return ok(news);
       } catch (error) {
         if(!error.status) error.status = 500
