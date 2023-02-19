d221363d23f10b597df529ed5a2a675544a54072
Caique0Araujo <caique.ara19@gmail.com>
2023-02-19 14:30:24 -0300

    add more accepted image types: add default type

--- src/helpers/image/imageExtension.ts
+++ src/helpers/image/imageExtension.ts
@@ -2,4 +2,5 @@ export default function getImageExtensionThroughType(imageType){
     if(imageType == 'image/png') return '.png'
     if(imageType == 'image/jpg') return '.jpg'
     if(imageType == 'image/jpeg') return  '.jpeg'
+    return '.png'
 }
